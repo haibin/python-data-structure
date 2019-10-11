ebb084b409012107923afd793e55aa1a40a823a3
Liu Haibin <liu.haibin@gmail.com>
2019-10-11 23:13:22 +0800

    Add stack

---
+++ stack.py
@@ -0,0 +1,53 @@
+class Stack:
+    def __init__(self):
+        self.array = list()
+
+    def push(self, val):
+        self.array.append(val)
+
+    def pop(self):
+        if len(self.array) == 0:
+            return None
+
+        index = len(self.array) - 1
+        return self.array.pop(index)
+
+    def len(self):
+        return len(self.array)
+
+
+def is_valid(s):
+    """ Returns boolean
+    """
+    stack = Stack()
+    for x in s:
+        if x in ['(', '[', '{']:
+            stack.push(x)
+            continue
+        if x in [')', ']', '}']:
+            val = stack.pop()
+            if val is None:
+                return False
+            if val == '(' and x != ')':
+                return False
+            if val == '[' and x != ']':
+                return False
+            if val == '{' and x != '}':
+                return False
+            continue
+    # check the stack
+    if stack.len() == 0:
+        return True
+    return False
+
+s = '( var x = { y: [1, 2, 3] } )'
+
+print(is_valid(s))
+
+s = '( var x = { y: [1, 2, 3] ) }'
+
+print(is_valid(s))
+
+s = '( var x = { y: [1, 2, 3] }'
+
+print(is_valid(s))
